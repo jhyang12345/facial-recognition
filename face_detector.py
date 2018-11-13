b10971785644a64080f8710176707b264cc62908
jhyang12345 <jhyang12345@naver.com>
2018-11-13 20:11:52 +0900

    Implemented find_face_locations function

--- face_detector.py
+++ face_detector.py
@@ -1,18 +1,28 @@
 import os, sys
 from PIL import Image
+import face_recognition
 import numpy as np
 from argparse import ArgumentParser
 
+def find_face_locations(image_path):
+    print(image_path)
+    image = face_recognition.load_image_file(image_path)
+    face_locations = face_recognition.face_locations(image,
+            number_of_times_to_upsample=0, model="cnn")
+    print(face_locations)
+
 def main(argv):
-    print(argv)
     parser = ArgumentParser()
     parser.add_argument("-p", "--path", required=False)
     args = vars(parser.parse_args())
+    path = ""
     if args["path"]:
         print("Path accepted")
-        path = args["path"][-1]
+        path = args["path"].split()[-1]
     else:
         print("No path given!")
+        return
+    find_face_locations(path)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
