92a25e8ab8c06c1c76fe71f2fe8b823af1314a89
jhyang12345 <jhyang12345@naver.com>
2018-11-14 00:05:35 +0900

    Markdown update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # facial-recognition
+
+## face_detector:
+```bash
+python face_detector.py -p <Directory-With-Images>
+```
+
+## faces_from_video:
+```bash
+python faces_from_video.py -p <Path-To-Video>
+```
---
+++ convert_video_format.py
@@ -0,0 +1,14 @@
+import os, sys
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+def main(argv):
+    file_path = argv[-1]
+    original_video = VideoFileClip(file_path)
+    video_path = os.path.dirname(file_path)
+    video_name = os.path.basename(file_path).split(".")[0]
+    video_name = video_name + ".mkv"
+    original_video.write_videofile(os.path.join(video_path, video_name), codec="libvpx")
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
