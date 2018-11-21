307a3d43b868d8e2aae3efd4d79f9d2a12b5362e
jhyang12345 <jhyang12345@naver.com>
2018-11-21 13:44:52 +0900

    Created base cnn model
    looking into automating model structure design

--- models/cnn.py
+++ models/cnn.py
@@ -3,7 +3,7 @@ from keras.layers import Input, Convolution2D, SeparableConvolution2D, \
                 Dense, Activation, BatchNormalization
 from keras.models import Sequential, Model
 
-class DeepDog:
+class CNN:
     def __init__(self, input_shape=(128, 128, 3)):
         self.image_width = input_shape[0]
         self.image_height = input_shape[1]
@@ -21,54 +21,52 @@ class DeepDog:
 
         # input format will usually be 128 or 2^7
         # strides of 2 halfs input shape
+        # usually kernel sizes are in odd numbers
         x = Convolution2D(int(32 * alpha), (3, 3), strides=(2, 2), padding='same')(model_input)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        # why isn't separable convolution used as firstlayer
-        x = SeparableConvolution2D(int(32 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
+        x = Convolution2D(int(64 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        x = SeparableConvolution2D(int(64 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
+        x = Convolution2D(int(64 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        x = SeparableConvolution2D(int(128 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
+        x = Convolution2D(int(128 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        x = SeparableConvolution2D(int(128 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
+        x = Convolution2D(int(128 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        x = SeparableConvolution2D(int(256 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
+        x = Convolution2D(int(256 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        x = SeparableConvolution2D(int(256 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
+        x = Convolution2D(int(256 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation(activation_type)(x)
 
-        for _ in range(5):
-            x = self.apply_separable_layer(x, int(512 * alpha), activation_type)
+        x = Convolution2D(int(512 * alpha), (3, 3), strides=(1, 1), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation(activation_type)(x)
+
+        x = Convolution2D(int(512 * alpha), (3, 3), strides=(2, 2), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation(activation_type)(x)
 
         # GlobalAveragePooling susceptible to change
         x = GlobalAveragePooling2D()(x)
         # output activation type is subject to change
         out = Dense(1, activation='sigmoid')(x)
 
-        self.model = Model(model_input, out, name='deepdog')
+        self.model = Model(model_input, out, name='cnn')
         # not sure what type of optimizer or loss function to use
         self.model.compile(loss='binary_crossentropy', optimizer='adam',
                                 metrics=['accuracy'])
 
-    # for this model type kernel size will be equal to 3
-    def apply_separable_layer(self, x, filters, activation_type, strides=(1, 1)):
-        x = SeparableConvolution2D(filters, (3, 3), strides=strides, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation(activation_type)(x)
-        return x
-
 if __name__ == '__main__':
-    deep_dog = DeepDog()
+    cnn = CNN()
